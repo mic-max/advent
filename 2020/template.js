8fb42ad5d57f5afbe6090c56937fa90c27a99ed9
Michael Maxwell <mike.ian.maxwell@gmail.com>
2020-12-08 20:48:05 -0800

    adding loop over each line

--- 2020/template.js
+++ 2020/template.js
@@ -3,6 +3,10 @@ const util = require('./util.js');
 (function() {
 	let input = util.readLines('input/dayXX.txt')
 
+	for (let line of input) {
+		
+	}
+
 	console.log() // 
 	console.log() // 
 }())
