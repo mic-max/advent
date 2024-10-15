2fb22c1ea401db442574b1e419670fce5639e442
Michael Maxwell <mike.ian.maxwell@gmail.com>
2024-10-15 00:35:13 -0400

    fixup 2023 day 1

--- 2023/d01.py
+++ 2023/d01.py
@@ -1,26 +1,119 @@
-from sys import stdin
+import math
+import re
+import statistics
+import sys
+import time
 
-digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
-          'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+import ahocorasick
+import num2words
 
-def first_appear(x, search_strs):
-    min_idx = -1
-    min_pos = 100000000
-    for idx, s in enumerate(search_strs):
-        position = x.find(s)
-        if position != -1 and position < min_pos:
-            min_idx = idx
-            min_pos = position
-    return min_idx
+P1 = 54634
+P2 = 53855
 
-sum = 0
-for line in stdin:
-    line = line.strip()
+def simple(L, part_two = False):
+    res = 0
+    if part_two:
+        digits = list('123456789')
+        for i in range(1, 10):
+            digits.append(num2words.num2words(i))
+        for line in L:
+            min_value = 0
+            min_pos = len(line) - 1
+            max_value = 0
+            max_pos = 0
 
-    n1 = first_appear(line, digits)
-    sum += 10 * (n1 % 9) + 1
+            for i, num in enumerate(digits):
+                if (pos := line.find(num, 0, min_pos + 1)) != -1:
+                    min_pos = pos
+                    min_value = (i % 9) + 1
 
-    n2 = first_appear(line[::-1], [x[::-1] for x in digits])
-    sum += (n2 % 9) + 1
+                if (pos2 := line.rfind(num, max_pos)) != -1:
+                    max_pos = pos2
+                    max_value = (i % 9) + 1
 
-print(sum)
+            res += min_value * 10
+            res += max_value
+    else:
+        for line in L:
+            for c in line:
+                if c.isdigit():
+                    res += (ord(c) - 48) * 10
+                    break
+
+            for c in reversed(line):
+                if c.isdigit():
+                    res += ord(c) - 48
+                    break
+    return res
+
+def regex(L, part_two = False):
+    nums = {}
+    for i in range(1, 10):
+        nums[str(i)] = i
+        if part_two:
+            nums[num2words.num2words(i)] = i
+
+    res = 0
+    for line in L:
+        m = re.search("|".join(nums.keys()), line)
+        res += nums[m[0]] * 10
+
+        m = re.search("|".join([x[::-1] for x in nums.keys()]), line[::-1])
+        res += nums[m[0][::-1]]
+    return res
+
+def aho_corasick(L, part_two = False):
+    a1 = ahocorasick.Automaton()
+    a2 = ahocorasick.Automaton()
+
+    for i in range(1, 10):
+        a1.add_word(str(i), i)
+        a2.add_word(str(i), i)
+    
+        if part_two:
+            word = num2words.num2words(i)
+            a1.add_word(word, i)
+            a2.add_word(word[::-1], i)
+
+    a1.make_automaton()
+    a2.make_automaton()
+
+    res = 0
+    for line in L:
+        for _, x in a1.iter(line):
+            res += x * 10
+            break
+
+        for _, x in a2.iter(line[::-1]):
+            res += x
+            break
+
+    return res
+
+def run(name, function, fastest = None, part_two = False):
+    start = time.perf_counter()
+    res = function(L, part_two)
+    elapsed = time.perf_counter() - start
+    print(f"| {name:15} | {elapsed:.6f} | {1 if not fastest else elapsed / fastest:.2f} |")
+    assert res == (P2 if part_two else P1)
+    return elapsed
+
+if __name__ == "__main__":
+    L = [line.strip() for line in sys.stdin]
+
+    print(f"Average Length: {statistics.mean([len(x) for x in L])}")
+    print(f"Median Length: {statistics.median([len(x) for x in L])}")
+    print(f"Min Length: {min([len(x) for x in L])}")
+    print(f"Max Length: {max([len(x) for x in L])}")
+
+    print("| Part 1      | Time     | Times Slower |")
+    print("| ------------ | -------- | ------------ |")
+    fastest_p1 = run("Simple", simple, None)
+    run("Aho-Corasick", aho_corasick, fastest_p1)
+    run("Regex", regex, fastest_p1)
+
+    print("| Part 2      | Time     | Times Slower |")
+    print("| ------------ | -------- | ------------ |")
+    fastest_p2 = run("Aho-Corasick", aho_corasick, None, True)
+    run("Regex", regex, fastest_p2, True)
+    run("Simple", simple, fastest_p2, True)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# advent
-Advent of Code Solutions
+# Advent of Code Solutions
+
+## Usage
+1. `pip install -r requirements.txt`
+1. Install `https://github.com/GreenLightning/advent-of-code-downloader`
+1. `aocdl -year 2023 -day 1`
+1. `Get-Content input-2023-1.txt | python 2023/d01.py`
