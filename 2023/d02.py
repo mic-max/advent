a87c82a9b1ef141690e533747ec9864afebf9de0
Michael Maxwell <mike.ian.maxwell@gmail.com>
2024-10-15 02:16:01 -0400

    Update d02.py

--- 2023/d02.py
+++ 2023/d02.py
@@ -1,30 +1,67 @@
-from sys import stdin
-from operator import mul
-from functools import reduce
+import collections
+import functools
+import operator
+import sys
+import statistics
+import time
 
-result = 0
-max_counts = { 'red': 12, 'green': 13, 'blue': 14 }
+P1 = 2348
+P2 = 76008
 
-for idx, line in enumerate(stdin, 1):
-    good_so_far = True
-    min_count = { 'red': 0, 'green': 0, 'blue': 0 }
+def part1(L, part_two = False):
+    res = 0
+    max_counts = { 'red': 12, 'green': 13, 'blue': 14 }
 
-    for setx in line[line.index(':') + 1:].strip().split(';'):
-        for pair in setx.strip().split(','):
-            count = int(pair.strip().split()[0])
-            color = pair.strip().split()[1]
+    for i, line in enumerate(L, 1):
+        skip_game = False
+        for setx in line[line.index(':') + 1:].strip().split(';'):
+            if skip_game:
+                break
+            for pair in setx.strip().split(','):
+                parts = [x.strip() for x in pair.split()]
+                count, colour = int(parts[0]), parts[1]
 
-            min_count[color] = max(count, min_count[color])
+                if count > max_counts[colour]:
+                    skip_game = True
+                    break
 
-            if count > max_counts[color]:
-                good_so_far = False
+        if not skip_game:
+            res += i
+    return res
 
-    # part 2.
-    power_value = reduce(mul, min_count.values(), 1)
-    result += power_value # pt 2
+def part2(L, part_two = False):
+    res = 0
 
-    # part 1.
-    # if good_so_far:
-    #     result += idx
+    for idx, line in enumerate(L, 1):
+        counts = collections.defaultdict(int)
 
-print(result)
+        for setx in line[line.index(':') + 1:].strip().split(';'):
+            for pair in setx.strip().split(','):
+                parts = [x.strip() for x in pair.split()]
+                count, colour = int(parts[0]), parts[1]
+                counts[colour] = max(count, counts[colour])
+
+        res += functools.reduce(operator.mul, counts.values(), 1)
+    return res
+
+def run(name, function, fastest = None, part_two = False):
+    start = time.perf_counter()
+    res = function(L, part_two)
+    elapsed = time.perf_counter() - start
+    print(f"| {name:15} | {elapsed:.6f} |")
+    assert res == (P2 if part_two else P1)
+    return elapsed
+
+if __name__ == "__main__":
+    L = [line.strip() for line in sys.stdin]
+    
+    print(f"Average Reveals: {statistics.mean([x.count(';') for x in L])}")
+    print(f"Median Reveals: {statistics.median([x.count(';') for x in L])}")
+    print(f"Min Reveals: {min([x.count(';') for x in L])}")
+    print(f"Max Reveals: {max([x.count(';') for x in L])}")
+    
+    print("| Part Number  | Time     |")
+    print("| ------------ | -------- |")
+    p1_elapsed = run("Part 1", part1, None)
+    p2_elapsed = run("Part 2", part2, None, True)
+
