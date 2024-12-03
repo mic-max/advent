249ade6afb5f181584b7f682825c377c2a1bc95f
Michael Maxwell <mike.ian.maxwell@gmail.com>
2024-12-03 00:45:29 -0500

    Create d03.py
    omg i really blew it on part 2 by not joining all lines of the input so I was incorrectly re-enabling multication at the beginning of every line...

---
+++ 2024/d03.py
@@ -0,0 +1,39 @@
+# Python
+import re
+
+# Local
+import util
+
+def simple_1(L):
+    res = 0
+    for line in L:
+        for m in re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', line):
+            x = int(m.group(1))
+            y = int(m.group(2))
+            res += x * y
+
+    return res
+
+def simple_2(L):
+    res = 0
+    
+    L = "".join(L)
+    for m in re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', "".join(L)):
+        x = int(m.group(1))
+        y = int(m.group(2))
+
+        i = m.start()
+        recent_do = L.rfind("do()", 0, i)
+        recent_dont = L.rfind("don't()", 0, i)
+        if recent_dont == -1 or recent_do > recent_dont:
+            res += x * y
+
+    return res
+
+if __name__ == '__main__':
+    L = util.strip_stdin()
+    results = util.run(L, 1, 0, [simple_1])
+    results += util.run(L, 2, 0, [simple_2])
+    util.print_table(results)
+
+
